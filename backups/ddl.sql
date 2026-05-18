b44a58a060a580c73205bd24745663a1f9ed0c28
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2026-05-18 17:18:03 +0900

    IL-1: added ddl

--- backups/ddl.sql
+++ backups/ddl.sql
@@ -1,18 +1,18 @@
--- public."Transcribation" definition
+-- public."transcription" definition
 
 -- Drop table
 
--- DROP TABLE public."Transcribation";
+-- DROP TABLE public."transcription";
 
-CREATE TABLE public."Transcribation" (
+CREATE TABLE public."transcription" (
 	id text NOT NULL,
-	"fileName" text NULL,
+	name text NULL,
 	code text NULL,
-	"createdAt" timestamp(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	"created_at" timestamp(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	"order" int4 DEFAULT 0 NULL,
 	tags _text NULL,
 	"content" jsonb NULL,
 	"section" text NULL,
-	"transcriptionText" text NULL,
-	CONSTRAINT "Transcribation_pkey" PRIMARY KEY (id)
+	"transcription_text" text NULL,
+	CONSTRAINT "transcription_pkey" PRIMARY KEY (id)
 );
